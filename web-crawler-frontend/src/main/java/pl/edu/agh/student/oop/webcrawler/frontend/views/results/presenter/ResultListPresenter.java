01d1b77b55bdda31763e3d966c67e38fcb6d603c
Kamil Jarosz <kamil09875@gmail.com>
2018-12-08 00:39:18 +0100

    Add example mock

--- web-crawler-frontend/src/main/java/pl/edu/agh/student/oop/webcrawler/frontend/views/results/presenter/ResultListPresenter.java
+++ web-crawler-frontend/src/main/java/pl/edu/agh/student/oop/webcrawler/frontend/views/results/presenter/ResultListPresenter.java
@@ -1,15 +1,20 @@
 package pl.edu.agh.student.oop.webcrawler.frontend.views.results.presenter;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
+import javafx.util.Duration;
+import pl.edu.agh.student.oop.webcrawler.frontend.views.results.model.ImmutableResult;
 import pl.edu.agh.student.oop.webcrawler.frontend.views.results.model.Result;
 import pl.edu.agh.student.oop.webcrawler.frontend.views.results.model.ResultListCell;
 
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.time.LocalDateTime;
+import java.util.Random;
 
 public class ResultListPresenter {
     @FXML
@@ -19,11 +24,25 @@ public class ResultListPresenter {
     public void initialize() throws URISyntaxException {
         list.setCellFactory(ResultListCell::new);
 
-        ObservableList<Result> items = FXCollections.observableArrayList(Result.builder()
+        ImmutableResult result = Result.builder()
                 .matchedText("asdf")
                 .creationDate(LocalDateTime.now())
                 .source(new URI("http://google.com"))
-                .build());
+                .build();
+        ObservableList<Result> items = FXCollections.observableArrayList(result);
+
+        Random random = new Random();
+        Timeline timeline = new Timeline(new KeyFrame(
+                Duration.millis(random.nextInt(5000) + 1000),
+                ae -> {
+                    items.add(0, Result.builder()
+                            .from(result)
+                            .creationDate(LocalDateTime.now())
+                            .build());
+                }));
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.play();
+
         list.setItems(items);
     }
 }
