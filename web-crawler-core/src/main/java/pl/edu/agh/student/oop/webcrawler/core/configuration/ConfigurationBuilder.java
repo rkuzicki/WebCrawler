73e9b145007e6d90b93c032e433cd9d51f4ea481
Kamil Jarosz <kamil09875@gmail.com>
2018-12-11 00:52:26 +0100

    Fix ConfigurationBuilder

--- web-crawler-core/src/main/java/pl/edu/agh/student/oop/webcrawler/core/configuration/ConfigurationBuilder.java
+++ web-crawler-core/src/main/java/pl/edu/agh/student/oop/webcrawler/core/configuration/ConfigurationBuilder.java
@@ -2,6 +2,7 @@ package pl.edu.agh.student.oop.webcrawler.core.configuration;
 
 import pl.edu.agh.student.oop.webcrawler.core.matcher.Matcher;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.OptionalInt;
@@ -9,7 +10,7 @@ import java.util.OptionalInt;
 public class ConfigurationBuilder {
     private Matcher matcher = null;
     private OptionalInt depth = OptionalInt.empty();
-    private List<String> domains;
+    private List<String> domains = new ArrayList<>();
 
     ConfigurationBuilder() {
 
@@ -20,8 +21,8 @@ public class ConfigurationBuilder {
         return this;
     }
 
-    public ConfigurationBuilder domains(List<String> domains) {
-        this.domains = domains;
+    public ConfigurationBuilder addDomain(String domain) {
+        this.domains.add(domain);
         return this;
     }
 
